9b93d4d9f1fd5a5e6160c9b324f828afa9908911
DiangD <1304138666@qq.com>
2021-02-07 22:06:11 +0800

    增加hello eggFS

--- server/server.go
+++ server/server.go
@@ -5,21 +5,29 @@ import (
 	"eggFs/logger"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"net/http"
 )
 
 func config() *conf.GlobalConfig {
 	return conf.Config()
 }
 
+func hello(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "hello eggFs!",
+	})
+}
+
 func Start() {
 	conf.ParseConfig(conf.ConfigFilePath)
-	router := gin.New()
-	router.Use(gin.Recovery())
+	r := gin.Default()
 	//gin.SetMode(gin.ReleaseMode)
 
-	err := router.Run(config().Port)
+	r.GET("/hello", hello)
+
+	err := r.Run(config().Port)
 	if err != nil {
-		logger.Panic("web服务启动失败", zap.String("port", "8080"))
+		logger.Panic("web服务启动失败", zap.String("port", config().Port))
 	}
-	logger.Info("web服务启动成功", zap.String("port", "8080"))
+	logger.Info("web服务启动成功", zap.String("port", config().Port))
 }
