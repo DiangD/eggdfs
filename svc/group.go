2ef2e04160bb07adbfe4f1fd406e756e1bdef55e
DiangD <1304138666@qq.com>
2021-02-27 22:05:28 +0800

    fix:group增加对写锁解决并发写

--- svc/group.go
+++ svc/group.go
@@ -3,6 +3,7 @@ package svc
 import (
 	"errors"
 	"sort"
+	"sync"
 )
 
 type Group struct {
@@ -10,6 +11,7 @@ type Group struct {
 	Status   int                      `json:"status"`
 	Cap      uint64                   `json:"cap"`
 	Storages map[string]StorageServer `json:"storages"`
+	mu       sync.RWMutex
 }
 
 type StorageServer struct {
@@ -20,11 +22,19 @@ type StorageServer struct {
 	UpdateTime int64  `json:"update_time"`
 }
 
-//GetStorages 获取注册的storage节点
+//GetStorages 获取注册的storage节点 map无法保证顺序
 func (g *Group) GetStorages() []StorageServer {
+	g.mu.RLock()
+	defer g.mu.RUnlock()
+	keys := make([]string, 0)
 	servers := make([]StorageServer, 0)
 	for _, server := range g.Storages {
-		servers = append(servers, server)
+		keys = append(keys, server.Addr)
+	}
+	//按addr排序
+	sort.Strings(keys)
+	for _, key := range keys {
+		servers = append(servers, g.Storages[key])
 	}
 	return servers
 }
@@ -34,6 +44,8 @@ func (g *Group) RegisterStorage(s StorageServer) error {
 	if s.Addr == "" {
 		return errors.New("storage addr can not be nil")
 	}
+	g.mu.Lock()
+	defer g.mu.Unlock()
 	if _, ok := g.Storages[s.Addr]; !ok {
 		g.Storages[s.Addr] = s
 	}
@@ -45,6 +57,8 @@ func (g *Group) RemoveStorage(s StorageServer) error {
 	if s.Addr == "" {
 		return errors.New("storage addr can not be nil")
 	}
+	g.mu.Lock()
+	defer g.mu.Unlock()
 	delete(g.Storages, s.Addr)
 	return nil
 }
@@ -54,6 +68,8 @@ func (g *Group) UpdateStorage(s StorageServer) error {
 	if s.Addr == "" {
 		return errors.New("storage addr can not be nil")
 	}
+	g.mu.Lock()
+	defer g.mu.Unlock()
 	if _, ok := g.Storages[s.Addr]; !ok {
 		return errors.New("nil storage")
 	}
@@ -63,6 +79,8 @@ func (g *Group) UpdateStorage(s StorageServer) error {
 
 //IsExistStorage storage节点是否注册
 func (g *Group) IsExistStorage(s StorageServer) bool {
+	g.mu.Lock()
+	defer g.mu.Unlock()
 	_, ok := g.Storages[s.Addr]
 	return ok
 }
@@ -72,12 +90,15 @@ func (g *Group) SaveOrUpdateStorage(s StorageServer) error {
 	if s.Addr == "" {
 		return errors.New("storage addr can not be nil")
 	}
+	g.mu.Lock()
+	defer g.mu.Unlock()
 	g.Storages[s.Addr] = s
 	return nil
 }
 
 //GetGroupCap 获取group容量 cap==minimum storage free
 func (g *Group) GetGroupCap() uint64 {
+	g.mu.RLock()
 	if len(g.Storages) == 0 {
 		return 0
 	}
@@ -85,6 +106,7 @@ func (g *Group) GetGroupCap() uint64 {
 	for _, v := range g.Storages {
 		caps = append(caps, v.Free)
 	}
+	g.mu.RUnlock()
 	sort.Slice(caps, func(i, j int) bool {
 		return caps[i] < caps[j]
 	})
